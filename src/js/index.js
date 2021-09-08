08fad1c3f8a87224c9fc48c34acab2dc7912bae8
Ayush Saini <ayushsainiiitb00@gmail.com>
2021-09-08 12:35:55 +0530

    boilerplate cleaned up

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,1 @@
 import '../styles/main.scss';
-
-const searchElem = document.querySelector('.search');
-const inputElem = document.querySelector('.input');
-
-inputElem.addEventListener('input', () => {
-  searchElem.innerHTML = inputElem.value;
-});
